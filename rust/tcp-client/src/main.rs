971aea616de45cdf19bfa7ae233bca87a30bdbf0
llComodino <comodinocode@proton.me>
2023-11-09 10:44:15 +0100

    Somewhat usable cli

--- rust/tcp-client/src/main.rs
+++ rust/tcp-client/src/main.rs
@@ -7,16 +7,69 @@ fn main() {
     
     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
     
-    let request = "GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n";
-    
-    let mut buffer = [0; 256];
-    let mut response = Response::new();
+    println!("Connected to the server!");
+    println!("1. Login");
+    println!("2. Register");
+    println!("3. Exit");
 
-    stream.write(request.as_bytes()).unwrap();
-    stream.read(&mut buffer).unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
 
-    response.body = String::from_utf8_lossy(&buffer).to_string();
+    match input {
 
-    println!("Response: {:?}", response);
+        "1" => {
+            println!("Enter your username: ");
+            let mut username = String::new();
+            std::io::stdin().read_line(&mut username).unwrap();
+            let username = username.trim();
 
+            println!("Enter your password: ");
+            let mut password = String::new();
+            std::io::stdin().read_line(&mut password).unwrap();
+            let password = password.trim();
+
+            let request = "POST /login HTTP/1.1\r\nHost:".to_string();
+            let mut buffer = [0; 512];
+
+            stream.write(request.as_bytes()).unwrap();
+            stream.read(&mut buffer).unwrap();
+            
+            let response = String::from_utf8_lossy(&buffer);
+
+            println!("{}", response);
+        },
+        
+        "2" => {
+            println!("Enter your username: ");
+            let mut username = String::new();
+            std::io::stdin().read_line(&mut username).unwrap();
+            let username = username.trim();
+
+            println!("Enter your password: ");
+            let mut password = String::new();
+            std::io::stdin().read_line(&mut password).unwrap();
+            let password = password.trim();
+
+            let request = "POST /register HTTP/1.1\r\nHost:".to_string();
+            let mut buffer = [0; 512];
+
+            stream.write(request.to_string().as_bytes()).unwrap();
+            stream.read(&mut buffer).unwrap();
+
+            let response_b = String::from_utf8_lossy(&buffer);
+
+            println!("{}", response_b);
+        },
+        
+        "3" => {
+            println!("Exiting...");
+            std::process::exit(0);
+        },
+        
+        _ => {
+            println!("Invalid input!");
+            std::process::exit(0);
+        }
+    }
 }
