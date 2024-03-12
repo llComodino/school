ebf7bea15304e768a9124bfed7b6d98d0742530f
llComodino <comodinocode@proton.me>
2024-03-12 13:23:16 +0100

    Sync Tue Mar 12 01:23:16 PM CET 2024

---
+++ c/quarta/var_len_args/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+double avg (int i, ...);
+
+int main (void) {
+
+    double n1 = 2;
+    double n2 = 4;
+    double n3 = 6;
+    double n4 = 8;
+
+    printf("Avg is: %.2f\n", avg(4, n1, n2, n3, n4));
+
+    return 0;
+}
+
+double avg (int i, ...) {
+
+    double avg = 0;
+    va_list arg_list;
+
+    va_start(arg_list, i);
+
+    for (size_t idx = 0; (int)(idx) < i; idx++) {
+
+        avg += va_arg(arg_list, double);
+
+    }
+
+    va_end(arg_list);
+
+    return (avg / i);
+}
