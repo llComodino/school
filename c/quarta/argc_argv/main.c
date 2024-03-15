1149b266f4d8f47f7c5b4472609f60479a5805f5
llComodino <comodinocode@proton.me>
2024-03-15 09:47:42 +0100

    Sync Fri Mar 15 09:47:42 AM CET 2024

---
+++ c/quarta/argc_argv/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+double power(double, double);
+double todouble (char *);
+
+int main (int argc, char **argv) {
+    
+    double sum = 0;
+
+    for (size_t i = 1; i < argc; i++) {
+
+        sum += todouble(*(argv + i));
+
+    }
+
+    printf("%.2f\n", sum);
+
+    return 0;
+}
+
+double power(double base, double exp) {
+
+    if (exp == 0) {
+
+        return 1;
+
+    }
+
+    for (size_t i = 1; i < exp; i++) {
+
+        base *= base;
+
+    }
+
+    return base;
+}
+
+double todouble (char * number) {
+
+    double num = 0;
+    double tmp = 0;
+    
+    for (size_t i = 0; i < strlen(number); i++) {
+ 
+        tmp = ( (double)(*(number + i)) - 48 );
+        num += ( tmp * power(10, strlen(number) - i - 1) );
+    
+    }
+
+    return num;
+
+}
