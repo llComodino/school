e298b2d2118978ceb03cb221352d88d6b597b320
llComodino <comodinocode@proton.me>
2025-02-13 19:16:40 +0100

    finished c implementation

--- c/sockets/sum/main.c
+++ c/sockets/sum/main.c
@@ -1,10 +1,13 @@
 #include <arpa/inet.h>
+#include <limits.h>
+#include <ctype.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <threads.h>
 
 int
 main (int argc, char **argv)
@@ -41,12 +44,38 @@ main (int argc, char **argv)
     {
       char *buffer = (char *)malloc (sizeof (char) * 256);
 
-      while (recv (client, buffer, 256, 0))
-        {
-          printf ("received: %s\n", buffer);
-        }
-    }
+      send (client, "Enter two integers:\0", (size_t)20, 0);
+      int non_numeric = 0;
+      int c = 0;
 
-  send (client, "connected\0", 10, 0);
-  return 0;
+      int n[2] = {0};
+      do
+        {
+          non_numeric = 0;
+          recv (client, buffer, 256, 0);
+          *(buffer + strlen(buffer) - 1) = '\0';
+          for (char *cur = buffer; *cur; cur++)
+            {
+              if (!isdigit (*cur))
+                {
+                  non_numeric = 1;
+                }
+            }
+          if (!non_numeric) {
+            char *endptr;
+            n[c++] = strtol(buffer, &endptr, 10); // Convert string to int (base 10)
+
+            if (*endptr != '\0') {
+              printf("Conversion failed: invalid characters in input\n");
+              c--;
+            }
+          }
+        }
+      while (non_numeric || c < 2);
+
+      char *sum = (char*)malloc(sizeof(char) * 32);
+      snprintf(sum, 32, "sum is: %d", n[0] + n[1]);
+      send (client, sum, strlen(sum) + 1, 0);
+      return 0;
+    }
 }
