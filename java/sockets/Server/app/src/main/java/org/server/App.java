af4fc41456048f83ce2ac87977bd53b09408195e
llComodino <comodinocode@proton.me>
2025-02-25 09:01:20 +0100

    sync

--- java/sockets/Server/app/src/main/java/org/server/App.java
+++ java/sockets/Server/app/src/main/java/org/server/App.java
@@ -2,19 +2,11 @@ package org.server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 public class App {
-    protected static String name = "GilaServer";
-    protected static int port = 8080;
-    protected static String address = "localhost";
-    protected static String content = new String(
-        "{"+
-        "\"name\":\"" + name + "\"," +
-        "\"addr\":\"" + address + "\"," +
-        "\"port\":" + port +
-        "}"
-    );
-
+    static String content = new String("");
+    static final int port = 8080;
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(port)) {
             System.out.println("Listening for connections...");
@@ -23,23 +15,48 @@ public class App {
             System.out.println("Client connected!");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String message = in.readLine();
-            System.out.println("Client says: " + message);
-
-            String response = new String(
-                "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: application/json\r\n" +
-                "Content-Length: " + content.length() + "\r\n" +
-                "\r\n" +
-                content
-            );
             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-            out.println(response);
+            String message = new String("");
+            while ( message.toLowerCase().equals("exit") ) {
+                message = in.readLine();
+
+                String response = new String(
+                    "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: application/json\r\n"
+                );
+
+                if ( message.toLowerCase().equals("weather") ) {
+
+                    content += "{";
+                    content += "\"weather\": \"" + getRandomWeather() + "\",";
+                    content += "\"temperature\": " + getRandomTemperature();
+                    content += "}\r\n";
+
+                    response += "Content-Length: " + content.length() + "\r\n";
+                    response += "\r\n" + content;
+
+                    out.println(response);
+                } else {
+                    out.println("Error, valid commands are: `weather` and `exit`");
+                }
+            }
 
             client.close();
             server.close();
         } catch (IOException e) {
             System.err.println(e);
         }
+    }
+
+    static String getRandomWeather() {
+      String[] weathers = { "Sunny", "Windy", "Rainy" };
+      Random rnd = new Random();
+
+      return weathers[ rnd.nextInt(3) ];
+    }
+
+    static int getRandomTemperature() {
+      Random rnd = new Random();
+      return rnd.nextInt(18,25);
     }
 }
